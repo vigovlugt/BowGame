fc82f09263d4ff4d5feae4b1532d460a1796bad5
Vigo Vlugt <vigovlugt@gmail.com>
2017-03-05 12:14:47 +0100

    first unity setup

---
+++ Assets/PlayerSetup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    private Behaviour[] turnOffs;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            return;
+        }
+
+        turnOff();
+    }
+
+    void turnOff()
+    {
+        Camera.main.enabled = false;
+
+        for (int i = 0; i < turnOffs.Length; i++)
+        {
+            turnOffs[i].enabled = false;
+
+        }
+    }
+}
